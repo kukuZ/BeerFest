094f008d53403ddc211afd5ac713ea7a748f65f5
motoki-Z <lazy3147@gamil.com>
2015-05-02 21:23:39 +0900

    Add viewsをカテゴリリストを返すように変更

--- mysite/cmp_index/views.py
+++ mysite/cmp_index/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from cmp_index.models import Category
 
 def category_list(request):
     '''カテゴリ一覧'''
-    return HttpResponse('カテゴリ')
+    #return HttpResponse('カテゴリ')
+    categories = Category.objects.all().order_by('id')
+    return render_to_response('cmp_index/categories_list.html',  # 使用するテンプレート
+                                {'categories': categories},       # テンプレートに渡すデータ
+                                context_instance=RequestContext(request))  # その他標準のコンテキスト
 
 def category_edit(request, category_id=None):
     '''カテゴリの編集'''
