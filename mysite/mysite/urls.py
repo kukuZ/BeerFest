e22b360e4bc22eb4dd89c175fe532d4919d8d120
motoki-Z <lazy3147@gmail.com>
2016-09-07 22:24:07 +0900

    add 画像追加

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
+
 
 urlpatterns = [
     # Examples:
@@ -9,4 +13,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cmp_index/', include('cmp_index.urls', namespace='cmp_index')),
     url(r'^cmp/', include('cmp.urls', namespace='cmp')),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
